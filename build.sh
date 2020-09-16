96c35d244f67a86d966bef71be60fb3deb114950
melvyn2 <melvyn.depeyrot@gmail.com>
2020-09-16 08:59:39 -1000

    Build.sh fixes

--- build.sh
+++ build.sh
@@ -10,18 +10,19 @@ if [ ! -f ./thirdparty/gperftools-2.0/.libs/libtcmalloc_minimal.so ]; then
   aclocal
   automake --add-missing
 	autoconf
-	./configure --enable-frame-pointers #--host=i686-unknown-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
-	make -j$CORES
+	./configure --enable-frame-pointers --host=i386 "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+#  sed -i 's/LIBTOOL = $(SHELL) $(top_builddir)\/libtool/LIBTOOL = libtool/' Makefile
+  make -j$CORES
 	cd ../..
 fi
 
-if [ ! -f ./thirdparty/protobuf-2.5.0/src/.libs/protoc ]; then
+if [ ! -f ./thirdparty/protobuf-2.5.0/src/.libs/libprotobuf.a ]; then
 	cd ./thirdparty/protobuf-2.5.0
   aclocal
   automake --add-missing
 	autoconf
 	chmod u+x autogen.sh
-	bash ./configure #--host=i686-unknown-linux-gnu "CFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0" "CXXFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0" "LDFLAGS=-m32" --enable-shared=no
+	bash ./configure --host=i386 "CFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0" "CXXFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0" "LDFLAGS=-m32" --enable-shared=no
 	make -j$CORES
 	cd ../..
 fi
@@ -32,7 +33,7 @@ if [ ! -f ./thirdparty/libedit-3.1/src/.libs/libedit.so ]; then
   automake --add-missing
 	autoconf
 	chmod u+x ./configure
-	bash ./configure #--host=i686-unknown-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+	bash ./configure --host=i386 "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
 	make -j$CORES
 	cd ../..
 fi
@@ -45,5 +46,5 @@ export VALVE_NO_AUTO_P4=1
 if [[ $1 == '-v' ]]; then
   make -f games.mak NO_CHROOT=1 MAKE_JOBS=1 MAKE_VERBOSE=1 "${@:2}"
 else
-  make -f games.mak NO_CHROOT=1 "$@"
+  make -f games.mak NO_CHROOT=1 MAKE_JOBS=$CORES "$@"
 fi
