26fb0440c47a36a40eb164c069a101471cbdd105
melvyn2 <melvyn.depeyrot@gmail.com>
2020-09-17 08:20:29 -1000

    Convert build.sh to tabs, remove unused script

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
-set -e  # Stop on error
+set -e	# Stop on error
 
 if [[ ! -v CORES ]]; then
-  CORES=`nproc`
+	CORES=`nproc`
 fi
 
 if [ ! -f ./thirdparty/gperftools-2.0/.libs/libtcmalloc_minimal.so ]; then
 	cd ./thirdparty/gperftools-2.0
-  aclocal
-  automake --add-missing
+	aclocal
+	automake --add-missing
 	autoconf
 	./configure --enable-frame-pointers "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
-  make -j$CORES
+	make -j$CORES
 	cd ../..
 fi
 
 if [ ! -f ./thirdparty/protobuf-2.5.0/src/.libs/libprotobuf.a ]; then
 	cd ./thirdparty/protobuf-2.5.0
-  aclocal
-  automake --add-missing
+	aclocal
+	automake --add-missing
 	autoconf
 	chmod u+x autogen.sh
 	./configure "CFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0" "CXXFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0" "LDFLAGS=-m32" --enable-shared=no
@@ -28,8 +28,8 @@ fi
 
 if [ ! -f ./thirdparty/libedit-3.1/src/.libs/libedit.so ]; then
 	cd ./thirdparty/libedit-3.1
-  aclocal
-  automake --add-missing
+	aclocal
+	automake --add-missing
 	autoconf
 	chmod u+x ./configure
 	./configure "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
@@ -50,7 +50,7 @@ fi
 
 export VALVE_NO_AUTO_P4=1
 if [[ $1 == '-v' ]]; then
-  make NO_CHROOT=1 MAKE_JOBS=1 MAKE_VERBOSE=1 -f games.mak "${@:2}"
+	make NO_CHROOT=1 MAKE_JOBS=1 MAKE_VERBOSE=1 -f games.mak "${@:2}"
 else
-  make NO_CHROOT=1 MAKE_JOBS=$CORES -f games.mak "${@:1}"
+	make NO_CHROOT=1 MAKE_JOBS=$CORES -f games.mak "${@:1}"
 fi
