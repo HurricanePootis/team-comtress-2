fdd64e9b1f66f26054767ef50e138e78c80e67af
HurricanePootis <eatabakhsh04@protonmail.com>
2020-09-22 08:53:08 -0500

    Fixed encoding on the download sh scripts

--- download_libs_mac.sh
+++ download_libs_mac.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-declare -a libs=("lib/common/osx32/libcryptopp.a")
-server="https://libs.mastercomfig.com/"
-
-for lib in "${libs[@]}"
-do
-  curl "${server}${lib}" -o "${lib}"
-done
+#!/bin/bashdeclare -a libs=("lib/common/osx32/libcryptopp.a")server="https://libs.mastercomfig.com/"for lib in "${libs[@]}"do  curl "${server}${lib}" -o "${lib}"done
